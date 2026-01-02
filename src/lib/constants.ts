9af722f283b74e85153c0fb686438e5797f72341
lazysince <lazysince87@gmail.com>
2026-01-02 17:23:35 -0500

    fixed social links

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,5 +3,5 @@ export const DOMAIN = "sasehacks.com";
 export const DATES = "March 7-8, 2026";
 export const CITY = "Gainesville, FL";
 
-export const DISCORD_INVITE = "https://discord.gg/4WwCK3cf";
+export const DISCORD_INVITE = "https://discord.gg/jHZADudePh";
 export const DEVPOST_URL = "https://sasehacks.devpost.com/";
